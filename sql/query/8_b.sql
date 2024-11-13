79b55b76f4be032cf6c0c8ae67a84c558a47ad33
ttontoey <65761088+ttontoey@users.noreply.github.com>
2024-11-13 17:14:08 +0700

    feat: item 8_b

--- sql/query/8_b.sql
+++ sql/query/8_b.sql
@@ -1,2 +1,24 @@
 -- 8.B) SQL DELETE
+-- Scenario: Seller table is referenced by Book, Post and Message
+-- Note that referential integrity MUST be maintained !
 
+-- First, try adding a new Seller
+-- This user is already a Buyer so UserInformation already exists
+-- The user is trying to create new Seller account which is seperated from the Buyer one
+INSERT INTO seller (email, password, ssn, username, profilepictureurl)
+VALUES (
+	'tontoeylnwzasellbook@gmail.com',
+	'78590657fe72bb2e9d87a3f4f89d47e48ecd28dd42660c0c1c7ddb3ac286825f',
+	'1112223334446',
+	'tontoeysellbook',
+	'https://cdn.anime-planet.com/characters/primary/hamtaro-1.jpg?t=1625777064'
+);
+
+SELECT * FROM seller;
+
+-- Working example
+DELETE FROM seller WHERE userid = 5;
+SELECT * FROM seller;
+
+-- Referential integrity violated example
+DELETE FROM seller WHERE userid = 1;
